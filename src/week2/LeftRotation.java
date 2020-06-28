02671c7f827f86de1165f6d9800cb2ff31c0b915
Eszter Viczian <eviczia@gmail.com>
2020-06-28 17:15:50 +0200

    Solved LeftRotation

--- src/week2/LeftRotation.java
+++ src/week2/LeftRotation.java
@@ -2,5 +2,28 @@
 
 package week2;
 
+import java.util.Arrays;
+
 public class LeftRotation {
+
+  public static void main(String[] args) {
+    int length = 5;
+    int rotations = 4;
+    int[] rotateThis = {1, 2, 3, 4, 5};
+
+    System.out.println(Arrays.toString(rotLeft(rotateThis, rotations)));
+
+  }
+
+  private static int[] rotLeft(int[] a, int d) {
+    int[] leftRotated = new int[a.length];
+
+    for (int i = 0; i < a.length; i++) {
+      int k = d + i - (d + i < a.length ? 0 : a.length);
+      leftRotated[i] = a[k];
+    }
+    return leftRotated;
+  }
+
+
 }
