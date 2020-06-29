5da20056d43850e30c1587cc3540d78c35ea6ac0
Eszter Viczian <eviczia@gmail.com>
2020-06-29 15:42:21 +0200

    Solved RansomNote

--- src/week2/RansomNote.java
+++ src/week2/RansomNote.java
@@ -2,5 +2,51 @@
 
 package week2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
+
+  public static void main(String[] args) {
+
+    String[] magazine = new String[]{"give", "me", "one", "grand", "today", "night"};
+    String[] note = new String[]{"give", "one", "grand", "today"};
+
+    checkMagazine(magazine, note);
+
+
+  }
+
+  private static void checkMagazine(String[] magazine, String[] note) {
+    Map<String, Integer> noteMap = textArrayToWordMap(note);
+    int end = magazine.length;
+    int i = 0;
+    while (!noteMap.isEmpty() && i < end) {
+      String current = magazine[i];
+      if (noteMap.containsKey(current)) {
+        if (noteMap.get(current) == 1) {
+          noteMap.remove(current);
+        } else {
+          noteMap.replace(current, noteMap.get(current) - 1);
+        }
+      }
+      i++;
+    }
+    System.out.println(noteMap.isEmpty() ? "Yes" : "No");
+  }
+
+
+  private static Map<String, Integer> textArrayToWordMap(String[] text) {
+    Map<String, Integer> dictionary = new HashMap<>();
+    for (String word : text) {
+      if (!(dictionary.containsKey(word))) {
+        dictionary.put(word, 1);
+      } else {
+        dictionary.replace(word, dictionary.get(word) + 1);
+      }
+    }
+    return dictionary;
+  }
+
+
 }
