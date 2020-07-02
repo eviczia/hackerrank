4e719fb57d75ae89b137fb444bf9faafe8e0b176
Eszter Viczian <eviczia@gmail.com>
2020-07-02 15:38:03 +0200

    diagonal difference solved

--- src/week3/DiagonalDifference.java
+++ src/week3/DiagonalDifference.java
@@ -1,20 +1,28 @@
 //https://www.hackerrank.com/challenges/diagonal-difference/problem
 package week3;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DiagonalDifference {
 
   public static void main(String[] args) {
+    List<List<Integer>> arr1 = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(9, 8, 9));
+    List<List<Integer>> arr2 = Arrays.asList(Arrays.asList(11, 2, 4), Arrays.asList(4, 5, 6), Arrays.asList(10, 8, -12));
+
+    System.out.println(diagonalDifference(arr1));
+    System.out.println(diagonalDifference(arr2));
 
   }
 
   public static int diagonalDifference(List<List<Integer>> arr) {
-    // Write your code here
-    return 0;
-
+    int dd = 0;
+    for (int i = 0; i < arr.size(); i++) {
+      List<Integer> currentLine = arr.get(i);
+      dd += currentLine.get(i) - currentLine.get(currentLine.size() - i - 1);
+    }
+    return Math.abs(dd);
   }
-
 }
 
 
